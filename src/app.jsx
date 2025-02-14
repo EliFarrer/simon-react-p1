43e2fdc71673e3702b3bc778632b80b042e04c9c
Eli Farrer <eli.farrer@gmail.com>
2025-02-13 20:47:52 -0700

    added navigation and not found function

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
 
 export default function App() {
     return (
+    <BrowserRouter>
       <div className="body bg-dark text-light">
         <header className="container-fluid">
           <nav className="navbar fixed-top navbar-dark">
             <div className="navbar-brand">Simon<sup>&reg;</sup></div>
             <menu className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" href="index.html">Home</a>
+                <NavLink className="nav-link" to="">Login</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="play.html">Play</a>
+                <NavLink className="nav-link" to="Play">Play</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="scores.html">Scores</a>
+                <NavLink className="nav-link" to="Scores">Scores</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="about.html">About</a>
+                <NavLink className="nav-link" to="About">About</NavLink>
               </li>
             </menu>
           </nav>
         </header>
     
-        <main>App components go here</main>
-    
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/play' element={<Play />} />
+            <Route path='/scores' element={<Scores />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>    
+
         <footer className="bg-dark text-white-50">
           <div className="container-fluid">
             <span className="text-reset">Author Name(s)</span>
@@ -34,5 +46,10 @@ export default function App() {
           </div>
         </footer>
       </div>
+    </BrowserRouter>
     )
 }
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
